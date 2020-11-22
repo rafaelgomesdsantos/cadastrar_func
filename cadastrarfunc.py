0161a3418af55ce2064dcaaafe0fd37206787ba9
Rafael Gomes dos Santos <74833822+rafaelgomesdsantos@users.noreply.github.com>
2020-11-22 00:39:36 -0300

    Update cadastrarfunc.py

--- cadastrarfunc.py
+++ cadastrarfunc.py
@@ -1,11 +1,11 @@
-#código 1 rafael data:
+#código 1 rafgds
 
 print ("------------Cadastro de Funcionário------------")
 
 nome = input ("Digite o nome do funcionário: ")
 empresa = input ("Digite a instituição: ")
 num_identificao = int(input("Digite o número de identificação: "))
-mediasalario = float(input("Digite a média da mensalidade: "))
+salario = float(input("Digite a média de salário: "))
 print (nome + " trabalha na empresa " + empresa)
 print ("Possui o número de identificação: ", num_identificao)
-print ("A média de salário é de: " + str(mediasalario))
+print ("A média de salário é de: " + str(salario))
